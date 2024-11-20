5b14c8becf0edce4cb756db3304ded229cb948f7
Ata Afshin <124207670+ataafshin@users.noreply.github.com>
2024-11-20 02:06:32 -0800

    User.js

--- models/User.js
+++ models/User.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
+// Define the User schema
 const UserSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    username: { type: String, required: true, unique: true },  // Unique username
+    password: { type: String, required: true },  // Password
 });
 
+// Export the User model
 module.exports = mongoose.model('User', UserSchema);
